da5a42d0b28d8534b6fb184e73285c7f710dad7c
Urong <dudwns519@daum.net>
2016-01-01 01:05:51 +0900

    Update TestMain.java

--- Two_dimension/src/com/urong/test/TestMain.java
+++ Two_dimension/src/com/urong/test/TestMain.java
@@ -33,11 +33,11 @@ class MyFrame extends JFrame {
 }
 
 class MyPanel extends JPanel {
-	int x = 1;
-	double[] y = 
-		{1, 1.2, 1.6, 2.6, 3.9, 5.2, 6.0, 3.2, 5.2, 3.1, 4.3, 3.3, 2.3, 2.5, 3.21, 3.2, 2.4, 2.1, 3, 3.2}; 
-	
 	protected void paintComponent(Graphics g) {
+		int x = 1;
+		double[] y = 
+			{1, 1.2, 1.6, 2.6, 3.9, 5.2, 6.0, 3.2, 5.2, 3.1, 4.3, 3.3, 2.3, 2.5, 3.21, 3.2, 2.4, 2.1, 3, 3.2}; 
+		
 		Graphics2D g2 = (Graphics2D) g;
 		for (double d : y) {
 			g2.fillOval(x+=1 * 20, (int) Math.round(d) * 20, 7, 7);

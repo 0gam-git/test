c593003a15d62e6222c422634089ee2e32191c00
june <june@LatisGlobal.net>
2016-01-29 16:21:11 +0900

    Spring security - dataSource Test

--- SpringTestSecurity/src/main/java/com/urong/contorller/HomeController.java
+++ SpringTestSecurity/src/main/java/com/urong/contorller/HomeController.java
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@Controller
+/*@Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
+	*//**
 	 * Simply selects the home view to render by returning its name.
-	 */
+	 *//*
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -34,6 +34,6 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
-	}
+		}
+}*/
 	
-}
--- SpringTestSecurity/src/main/java/com/urong/contorller/SecurityController.java
+++ SpringTestSecurity/src/main/java/com/urong/contorller/SecurityController.java
@@ -1,44 +1,39 @@
 package com.urong.contorller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.urong.dto.User;
-import com.urong.service.UserService;
 
 @Controller
 public class SecurityController {
 
-/*	@Autowired
-	private UserService userService;
-*/
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String login() {
+	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView defaultPage() {
 
-		return "login";
-	}
-	
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String root(){
-		return "login";
-	}
-
-	@RequestMapping(value = "/accessLogin", method = RequestMethod.POST)
-	public ModelAndView accessLogin(User user) {
 		ModelAndView mav = new ModelAndView();
-//		String id = user.getId();
-//		String pass = user.getPassword();
-//
-//		System.out.println("id== " + id);
-//		System.out.println("pass== " + pass);
-//
-////		User userInfo = userService.login(user);
-//
-//		mav.setViewName("/login");
-//		mav.addObject("userInfo", userInfo);
+		mav.addObject("title", "Spring Security Login Form");
+		mav.addObject("message", "DataSource Test");
+
+		mav.setViewName("hello");
+		return mav;
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		ModelAndView mav = new ModelAndView();
+		if (error != null) {
+			mav.addObject("error", "Invalid username and password!");
+		}
+
+		if (logout != null) {
+			mav.addObject("msg", "You've been logged out successfully.");
+		}
+		mav.setViewName("login");
 
 		return mav;
 	}
